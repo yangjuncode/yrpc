d7c89a6f7b4c63fd3687e3fb2b81e9ac96de2249
linfl-bfdx <linfl@bfdx.com>
2019-05-22 18:03:15 +0800

    rebuild demo proto

--- gen.sh
+++ gen.sh
@@ -2,6 +2,6 @@
 
 protoc --gogofaster_out=. yrpc.proto
 
-pbjs --no-create --no-verify --no-convert --no-delimited --force-long   -t static-module -w commonjs -o javascript/yrpc/rpc.js yrpc.proto
-pbts -o javascript/yrpc/rpc.d.ts javascript/yrpc/rpc.js
+pbjs --no-create --no-verify --no-convert --no-delimited --force-long   -t static-module -w commonjs -o javascript/yrpc/yrpc.js yrpc.proto
+pbts -o javascript/yrpc/yrpc.d.ts javascript/yrpc/yrpc.js
 
