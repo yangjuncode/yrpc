02a48eed8a8c9f90f9a052040211f770b64c8715
linfl-bfdx <linfl@bfdx.com>
2019-05-23 11:07:31 +0800

    fix target to es6

--- demo/gen.sh
+++ demo/gen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p demo/
-pbjs --no-create --no-verify --no-convert --no-delimited --force-long   -t static-module -w commonjs -o demo/demo.js demo.proto
+pbjs --no-create --no-verify --no-convert --no-delimited --force-long   -t static-module -w es6 -o demo/demo.js demo.proto
 pbts -o demo/demo.d.ts demo/demo.js
 
--- gen.sh
+++ gen.sh
@@ -2,6 +2,6 @@
 
 protoc --gogofaster_out=. yrpc.proto
 
-pbjs --no-create --no-verify --no-convert --no-delimited --force-long   -t static-module -w commonjs -o javascript/yrpc/yrpc.js yrpc.proto
+pbjs --no-create --no-verify --no-convert --no-delimited --force-long   -t static-module -w es6 -o javascript/yrpc/yrpc.js yrpc.proto
 pbts -o javascript/yrpc/yrpc.d.ts javascript/yrpc/yrpc.js
 
