09cb7be9abe19fc181ecda480f5ab45010af35e5
linfl-bfdx <linfl@bfdx.com>
2019-05-23 11:10:26 +0800

    rm old file

--- javascript/yrpc/dist/mapUtil.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function isCallbackInMap(key: string, callBack: Function, _map: Map<string, Function[]>): boolean;
-export declare function addCallback2Map(key: string, callBack: Function, _map: Map<string, Function[]>): void;
-export declare function delCallbackFromMap(key: string, callBack: Function, _map: Map<string, Function[]>): void;
